1f2719b439571701bdba9a5e9b23d80f0fb42e5a
RichardWang2127 <Wang.Richard2011@outlook.com>
2025-06-15 20:15:20 +0800

    Update sw.js

--- shmetro/assets/script/sw.js
+++ shmetro/assets/script/sw.js
@@ -1,21 +1,27 @@
-// sw.js
-const CACHE_NAME = 'shmetro-cache-v1';
+const CACHE_NAME = 'shmetro-cache-v2';
+
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/shmetro/assets/script/shmetro.js'
+  '/shmetro/assets/script/shmetro.js',
+//   '/favicon.svg',
+//   '/assets/favicon.ico',
+//   '/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
